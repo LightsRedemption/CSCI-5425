8982140eaf7d730ea9503872b3c8bdf8445c8f69
LightsRedemption <51008873+LightsRedemption@users.noreply.github.com>
2026-08-28 16:41:36 -0600

    Add files via upload

---
+++ assignment1/setup_script.py
@@ -0,0 +1,18 @@
+# %%
+import platform
+
+# %%
+print(f"I am running from Google's Servers: {platform.node()}" )
+
+# %% [markdown]
+# # Local  Test Sucess
+
+# %%
+name = input("Enter your name:")
+
+print(f"Happy Coding {name}!")
+
+# %%
+
+
+
